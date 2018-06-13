270f1f0e39e08b02d627ecb8f397f53d872a0b1d
Marek Fajkus <marek.faj@gmail.com>
2018-06-13 21:25:09 +0200

    add some comments

--- site.hs
+++ site.hs
@@ -1,3 +1,52 @@
+{-
+
+Copyright (c) 2016, Marek Fajkus
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MAREK FAJKUS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+My blog builder on top of Hakyll & Haskell
+
+ \    \\     \
+  \    \\     \
+   \    \\     \ _____________
+    \    \\     \\            \
+     \    \\     \\____________\
+      \    \\     \ _____________
+      /    //      \\            \
+     /    //        \\____________\
+    /    //    /\    \
+   /    //    /  \    \
+  /    //    /    \    \
+ /____//____/      \____\
+    _    _           _        _ _
+   | |  | |         | |      | | |
+   | |__| | __ _ ___| | _____| | |
+   |  __  |/ _` / __| |/ / _ \ | |
+   | |  | | (_| \__ \   <  __/ | |
+   |_|  |_|\__,_|___/_|\_\___|_|_|
+
+-}
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import System.Environment (getArgs)
@@ -9,6 +58,9 @@ import Hakyll.Web.Sass (sassCompiler)
 
 --------------------------------------------------------------------------------
 
+{-
+    This function handles context metadata for posts
+-}
 postCtx :: Tags -> Context String
 postCtx tags =
     mconcat
@@ -23,6 +75,11 @@ compressCssItem =
 
 -- Drafts
 
+{-
+    This function cleans cached preview (unpublished blogposts)
+    Preview are thing hacked o top of hakyll which makes it easier
+    to work on more changes posts in prallel when I want to.
+-}
 cleanPreview :: IO ()
 cleanPreview = do
     remove "generated/preview"
