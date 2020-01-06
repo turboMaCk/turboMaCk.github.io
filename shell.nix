7bb151fa46c4e1b827193b613960e39f3463ec57
Marek Fajkus <marek.faj@gmail.com>
2020-01-06 18:33:32 +0100

    update nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,12 +1,17 @@
 let
+  config = {
+    # hakyll-sass is marked as broken but works
+    # https://github.com/NixOS/nixpkgs/pull/77107
+    allowBroken = true;
+  };
   pkgs =
-    # nixpkgs 19.03 as of 09/09/2019
+    # nixpkgs unstable as of 06/01/2020
     import ((import <nixpkgs> {}).fetchFromGitHub {
         owner = "NixOS";
         repo = "nixpkgs-channels";
-        rev = "5b0b58685d68b9c9c12132f7b392c9dcdecc5931";
-        sha256 = "1fy1ghl79p3w1vg0hnakvrb1lqic4mjrwmylcgqa5bd4ilfrirzf";
-    }) {};
+        rev = "2e8fc97dbfacc1178ae846ab31210539f7a958f0";
+        sha256 = "1api5vypvw5pihz3kjps8q5hxf02ncgxg6axnpr2lcbc6m14s40q";
+    }) { inherit config; };
   site = import ./default.nix { inherit pkgs; };
 in
   pkgs.mkShell {
