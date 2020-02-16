33a9435e81e241ec2ea6251101e35b9a522e2793
Marek Fajkus <marek.faj@gmail.com>
2020-02-16 18:40:56 +0100

    remove unecessary hakyll.nix

--- default.nix
+++ default.nix
@@ -1,2 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.haskellPackages.callPackage ./site.nix {}
+let
+  config = {
+    packageOverrides = pkgs: rec {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+          hakyll = haskellPackagesOld.hakyll.overrideAttrs( old: {
+            configureFlags = "-f watchServer -f previewServer";
+            patches = [./hakyll.patch];
+          });
+          site =
+            haskellPackagesNew.callPackage ./site.nix {};
+        };
+      };
+    };
+  };
+  pkgs = import <nixpkgs> { inherit config; };
+in {
+  site = pkgs.haskellPackages.site;
+  env = pkgs.haskellPackages.site.env;
+  pkgs = pkgs;
+}
--- shell.nix
+++ shell.nix
@@ -1,19 +1,6 @@
 let
-  config = {
-    # hakyll-sass is marked as broken but works
-    # https://github.com/NixOS/nixpkgs/pull/77107
-    allowBroken = true;
-  };
-  pkgs =
-    # nixpkgs unstable as of 06/01/2020
-    import ((import <nixpkgs> {}).fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nixpkgs-channels";
-        rev = "2e8fc97dbfacc1178ae846ab31210539f7a958f0";
-        sha256 = "1api5vypvw5pihz3kjps8q5hxf02ncgxg6axnpr2lcbc6m14s40q";
-    }) { inherit config; };
-  site = import ./default.nix { inherit pkgs; };
+  vals = import ./default.nix;
 in
-  pkgs.mkShell {
-    buildInputs = [ site ];
-  }
+vals.pkgs.mkShell {
+  buildInputs = [ vals.site ];
+}
