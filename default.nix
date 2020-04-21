3ab6002466d6e7060705e88d5398ccef5fcada9f
Marek Fajkus <marek.faj@gmail.com>
2020-04-22 00:43:36 +0200

    update BIO

--- default.nix
+++ default.nix
@@ -1,14 +1,33 @@
 let
   config = {
-    packageOverrides = pkgs: {
+    packageOverrides = pkgs: rec {
+      libsass = pkgs.libsass.overrideAttrs (old: rec {
+        version = "3.6.3";
+        src = pkgs.fetchFromGitHub {
+            owner = "sass";
+            repo = old.pname;
+            rev = version;
+            url = "https://github.com/sass/libsass/archive/${version}.tar.gz";
+            sha256 = "1q6lvd8sj5k5an32qir918pa5khhcb8h08dzrg1bcxmw7a23j514";
+            # Remove unicode file names which leads to different checksums on HFS+
+            # vs. other filesystems because of unicode normalisation.
+            extraPostFetch = ''
+                rm -r $out/test/e2e/unicode-pwd
+            '';
+        };
+      });
+
       haskellPackages = pkgs.haskellPackages.override {
-        overrides = haskellPackagesNew: haskellPackagesOld: {
-          hakyll = haskellPackagesOld.hakyll.overrideAttrs(old: {
+        overrides = self: super: {
+          hlibsass = super.hlibsass.override {
+            inherit libsass;
+          };
+          hakyll = super.hakyll.overrideAttrs(old: {
             configureFlags = "-f watchServer -f previewServer";
             patches = [./hakyll.patch];
           });
           site =
-            haskellPackagesNew.callPackage ./site.nix {};
+            self.callPackage ./site.nix {};
         };
       };
     };
